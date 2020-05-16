b89cdf28d436539db10e0997cf179fb745d32041
Divyasahithya <61700828+Divyasahithya@users.noreply.github.com>
2020-05-17 04:28:54 +0530

    Changed

--- custo.js
+++ custo.js
@@ -1,4 +1,4 @@
-<script>
+
 //Get the button
 var mybutton = document.getElementById("myBtn");
 
@@ -18,4 +18,49 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-</script>
+function clickCounter1() {
+  if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.clickcount1) {
+      sessionStorage.clickcount1 = Number(sessionStorage.clickcount1)+1;
+    } else {
+      sessionStorage.clickcount1 = 1;
+    }
+    document.getElementById("result").innerHTML = "You have Ordered " + sessionStorage.clickcount1 + " rice item(s).";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
+
+function clickCounter2() {
+  if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.clickcount2) {
+      sessionStorage.clickcount2 = Number(sessionStorage.clickcount2)+1;
+    } else {
+      sessionStorage.clickcount2 = 1;
+    }
+    document.getElementById("result").innerHTML = "You have Ordered " + sessionStorage.clickcount2 + " meals.";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
+function clickCounter3() {
+  if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.clickcount3) {
+      sessionStorage.clickcount3 = Number(sessionStorage.clickcount3)+1;
+    } else {
+      sessionStorage.clickcount3 = 1;
+    }
+    document.getElementById("result").innerHTML = "You have Ordered " + sessionStorage.clickcount3 + " chapathi(s).";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+  var count=Number(sessionStorage.clickcount1)+Number(sessionStorage.clickcount2)+Number(sessionStorage.clickcount3);
+  document.getElementById("i").innerHTML = "You have Ordered " + count +" items.";
+  
+
+}
+var c1=document.getElementsByName("input").clickcount1;
+var c2=document.getElementsByName("input").clickcount2;
+var c3=document.getElementsByName("input").clickcount3;
+//receive.addEventListener("click",function (){
+	
